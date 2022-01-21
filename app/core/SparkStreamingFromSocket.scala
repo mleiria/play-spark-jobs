8011dd5989e83a25a6aef17c182b073cf8656e30
mleiria <manuel.leiria@gmail.com>
2022-01-21 17:23:25 +0000

    spark test 01

---
+++ app/core/SparkStreamingFromSocket.scala
@@ -0,0 +1,13 @@
+package core
+
+import org.apache.spark
+import org.apache.spark._
+import org.apache.spark.streaming._
+
+object SparkManager {
+
+  def run(): Unit ={
+    val df = spark.readStream.format()
+  }
+
+}
